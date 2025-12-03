de3cdb21d48831cc9c0990b3db67d912f7ffb34e
NikolaevaAR <nikolanastya2611@gmail.com>
2025-12-04 00:10:00 +0300

    Added class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
+    
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    MESSAGE_ADDING_TO_CART = (By.CSS_SELECTOR, "#messages :nth-child(1) .alertinner")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    NAME_PRODUCT_IN_MESSAGE = (By.CSS_SELECTOR, "#messages :nth-child(1) .alertinner strong")
+    MESSAGE_COST_PRODUCT = (By.CSS_SELECTOR, "#messages :nth-child(3) p")
+    COST_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
+    COST_PRODUCT_IN_MESSAGE = (By. CSS_SELECTOR, "#messages :nth-child(3) p strong")
