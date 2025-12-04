71ff55e6cf27b718afb8251e0f985605e0a6a08a
NikolaevaAR <nikolanastya2611@gmail.com>
2025-12-04 23:28:12 +0300

    Adde new methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,9 @@
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 
 class BasePage():
@@ -9,6 +13,13 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
         
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Ссылка логина отсутствует"
+    
     def open (self):
         self.browser.get(self.url)
         
@@ -32,3 +43,17 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+            
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+        
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
