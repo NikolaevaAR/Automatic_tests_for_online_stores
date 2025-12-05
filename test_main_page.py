2f64b6c9f6b774d0223852813b020a38d1f84e33
NikolaevaAR <nikolanastya2611@gmail.com>
2025-12-05 23:26:18 +0300

    Added new tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,12 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 from selenium.webdriver.common.by import By
+import pytest
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    
-    link = "http://selenium1py.pythonanywhere.com/"
-    
     page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()   # открываем страницу
     
@@ -16,10 +17,17 @@ def test_guest_can_go_to_login_page(browser):
     
     
 def test_guest_should_see_login_link(browser):
-    
-    link = "http://selenium1py.pythonanywhere.com/"
-    
     page = MainPage(browser, link)
     page.open()
     
     page.should_be_login_link()
+    
+   
+# @pytest.mark.new    
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser,browser.current_url)
+    basket_page.should_be_not_items_in_the_basket()
+    basket_page.should_be_a_text_the_basket_is_empty()
