91b9ceaf26ae7d49b2ab2f3fe8db6d9701ab1158
NikolaevaAR <nikolanastya2611@gmail.com>
2025-12-05 23:24:11 +0300

    Added new tests

--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,7 @@
 
 import pytest
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 link1 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -57,4 +58,13 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link1)
     page.open()
     page.go_to_login_page()
+    
+# @pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_not_items_in_the_basket()
+    basket_page.should_be_a_text_the_basket_is_empty()
     
