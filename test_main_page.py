eb0b0de106986f478b36d7149ed90defb43ac462
NikolaevaAR <nikolanastya2611@gmail.com>
2025-12-08 23:22:25 +0300

    Added class TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -6,23 +6,6 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()   # открываем страницу
-    
-    page.go_to_login_page()   # выполняем метод страницы — переходим на страницу логина
-    
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-    
-    
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    
-    page.should_be_login_link()
-    
-   
 # @pytest.mark.new    
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page = MainPage(browser, link)
@@ -31,3 +14,24 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser,browser.current_url)
     basket_page.should_be_not_items_in_the_basket()
     basket_page.should_be_a_text_the_basket_is_empty()
+    
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()   # открываем страницу
+        
+        page.go_to_login_page()   # выполняем метод страницы — переходим на страницу логина
+        
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+    
+    
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        
+        page.should_be_login_link()
+    
+   
